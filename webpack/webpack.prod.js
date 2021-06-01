7bb6d220a2839a46fb25821cfbad4564d8009490
wipaaa <ini.wipaaa@gmail.com>
2021-06-01 16:00:50 +0800

    Add css minimizer and extractor

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -9,6 +10,9 @@ const common = require('./webpack.common'); // the common webpack configuration
 module.exports = merge(common, {
   mode: 'production',
   output: { filename: '[name].[contenthash:8].bundle.js' },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
   plugins: [
     new HtmlPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
