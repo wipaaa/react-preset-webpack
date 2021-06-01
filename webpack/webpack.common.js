da1adf9683affd111821b6a9b8df6c366437e4f7
wipaaa <ini.wipaaa@gmail.com>
2021-06-01 14:51:35 +0800

    Add babel support

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const { ProgressPlugin } = require('webpack');
+
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
   output: {
@@ -7,6 +9,16 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.([jt]sx?)$/i,
+        include: /[\\/]src[\\/]/i,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [new ProgressPlugin()],
   resolve: {
     extensions: ['.css', '.sass', '.scss', '.js', '.jsx', '.ts', '.tsx'],
   },
