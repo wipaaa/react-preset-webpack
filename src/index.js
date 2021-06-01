772f1d5ccc69bebd5a1d7bc8ff464feb87ee9182
wipaaa <ini.wipaaa@gmail.com>
2021-06-01 14:59:54 +0800

    Add react support and basic react boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+
+render(
+  <StrictMode>
+    <h1>Hello from ReactJS</h1>
+  </StrictMode>,
+  document.getElementById('root')
+);
