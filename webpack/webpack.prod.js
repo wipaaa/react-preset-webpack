dc11e2114d86bca66bc3160843e9b50411654629
wipaaa <ini.wipaaa@gmail.com>
2021-06-01 15:45:39 +0800

    Fixing HMR not working in production

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,9 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
 const path = require('path');
 
 const HtmlPlugin = require('html-webpack-plugin');
+
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
